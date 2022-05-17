396589c4d4b8548517e2a4e96236da03598f3a00
Pavel Quevedo <pavel.quevedo@rivetlogic.com>
2022-05-17 16:18:42 -0600

    Organizing projects in the solution using Clean Architectue nomenclature.

--- CleanArchitecture.Domain/VideoActor.cs
+++ CleanArchitecture.Domain/VideoActor.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArchitecture.Domain.Common;
 
 namespace CleanArchitecture.Domain
 {
-    public class VideoActor
+    public class VideoActor : BaseDomainModel
     {
         public int VideoId { get; set; }
         public int ActorId { get; set; }
