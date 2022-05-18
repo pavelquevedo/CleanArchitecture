028398e68dcad0b6f965da276bb8ea887676da9b
Pavel Quevedo <pavel.quevedo@rivetlogic.com>
2022-05-18 11:56:51 -0600

    Adding IStreamerRepository and Streamer command and handler.

---
+++ CleanArchitecture.Application/Features/Streamers/Commands/StreamerCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Features.Streamers.Commands
+{
+    public class StreamerCommand : IRequest<int>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+
+    }
+}
---
+++ CleanArchitecture.Application/Features/Streamers/Commands/StreamerCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.Contracts.Infrastructure;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Models;
+using CleanArchitecture.Domain;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Features.Streamers.Commands
+{
+    public class StreamerCommandHandler : IRequestHandler<StreamerCommand, int>
+    {
+        private readonly IStreamerRepository _streamerRepository;
+        private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
+        private readonly ILogger<StreamerCommandHandler> _logger;
+
+        public StreamerCommandHandler(IStreamerRepository streamerRepository, IMapper mapper, IEmailService emailService, ILogger<StreamerCommandHandler> logger)
+        {
+            _streamerRepository = streamerRepository;
+            _mapper = mapper;
+            _emailService = emailService;
+            _logger = logger;
+        }
+
+        public async Task<int> Handle(StreamerCommand request, CancellationToken cancellationToken)
+        {
+            var streamerEntity = _mapper.Map<Streamer>(request);
+            var newStreamer = await _streamerRepository.AddAsync(streamerEntity);
+
+            _logger.LogInformation($"Streamer {newStreamer.Id} was created successfully");
+
+            return newStreamer.Id;
+        }
+
+        private async Task SendEmail(Streamer streamer)
+        {
+            var email = new Email
+            {
+                To = "pavelquevedo@outlook.com",
+                Body = "Streamer company was created successfully",
+                Subject = "Alert message"
+            };
+
+            try
+            {
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error sending mail of {streamer.Id} : {ex.Message}");
+            }
+            
+        }
+    }
+}
