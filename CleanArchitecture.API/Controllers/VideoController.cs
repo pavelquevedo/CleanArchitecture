4f6c4ea81623d807188e6b52dce9ed1da46d6e6e
Pavel Quevedo <pavel.quevedo@rivetlogic.com>
2022-05-19 13:06:41 -0600

    Adding Streamer and Video controllers.

---
+++ CleanArchitecture.API/Controllers/VideoController.cs
@@ -0,0 +1,34 @@
+﻿using CleanArchitecture.Application.Features.Videos.Queries.GetVideosList;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CleanArchitecture.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public VideoController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{username}", Name = "GetVideo")]
+        [ProducesResponseType(typeof(IEnumerable<VideoVm>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<VideoVm>>> GetVideosByUsername(string username)
+        {
+            var query = new GetVideosListQuery(username);
+            var videos = await _mediator.Send(query);
+
+            return Ok(videos);
+        }
+
+
+
+
+    }
+}
