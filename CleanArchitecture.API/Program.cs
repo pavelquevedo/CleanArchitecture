0b997da6c1268d4217a6865f67488223a44c3462
Pavel Quevedo <pavel.quevedo@rivetlogic.com>
2022-05-20 18:13:19 -0600

    Adding infrastructure dependency injections. Referencing application services and infrastructure services in WebApi.

--- CleanArchitecture.API/Program.cs
+++ CleanArchitecture.API/Program.cs
@@ -1,3 +1,6 @@
+using CleanArchitecture.Application.Extensions;
+using CleanArchitecture.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Adding infrastructure and application services by extension methods
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
