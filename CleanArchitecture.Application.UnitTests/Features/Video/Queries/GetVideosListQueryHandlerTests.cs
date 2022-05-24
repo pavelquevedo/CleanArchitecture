c30f30b4f32e2bde53b83e647e9bd392ec7fe755
Pavel Quevedo <pavel.quevedo@rivetlogic.com>
2022-05-24 09:22:09 -0600

    Implementing EntityFramework InMemory.

--- CleanArchitecture.Application.UnitTests/Features/Video/Queries/GetVideosListQueryHandlerTests.cs
+++ CleanArchitecture.Application.UnitTests/Features/Video/Queries/GetVideosListQueryHandlerTests.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using AutoMapper;
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Application.Features.Videos.Queries.GetVideosList;
+using CleanArchitecture.Application.Mapping;
+using CleanArchitecture.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace CleanArchitecture.Application.UnitTests.Features.Video.Queries
 {
     public class GetVideosListQueryHandlerTests
     {
+        private readonly IMapper _mapper;
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+
+        public GetVideosListQueryHandlerTests()
+        {
+            _unitOfWork = MockUnitOfWork.GetUnitOfWork();
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetVideoListTest()
+        {
+            //Arrange
+            var handler = new GetVideosListQueryHandler(_unitOfWork.Object, _mapper);
+            var request = new GetVideosListQuery("system");
+
+            //Act
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            //Assert
+            result.ShouldBeOfType<List<VideoVm>>();
+            result.Count.ShouldBe(1);
+        }
     }
 }
--- CleanArchitecture.Application.UnitTests/Mocks/MockVideoRepository.cs
+++ CleanArchitecture.Application.UnitTests/Mocks/MockVideoRepository.cs
@@ -1,7 +1,10 @@
 ﻿using AutoFixture;
-using CleanArchitecture.Application.Contracts.Persistence;
 using CleanArchitecture.Domain;
+using CleanArchitecture.Infrastructure.Persistence;
+using CleanArchitecture.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Moq;
+using System;
 using System.Linq;
 
 
@@ -9,13 +12,28 @@ namespace CleanArchitecture.Application.UnitTests.Mocks
 {
     public static class MockVideoRepository
     {
-        public static Mock<IVideoRepository> GetVideoRepository()
+        public static Mock<VideoRepository> GetVideoRepository()
         {
             var fixture = new Fixture();
+            
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
             var videos = fixture.CreateMany<Video>().ToList();
+            
+            //Adding fake data
+            videos.Add(fixture.Build<Video>()
+                .With(tr => tr.CreatedBy, "system").Create());
 
-            var mockRepository = new Mock<IVideoRepository>();
-            mockRepository.Setup(r => r.GetAllAsync()).ReturnsAsync(videos);
+            var options = new DbContextOptionsBuilder<StreamerDbContext>()
+                .UseInMemoryDatabase(databaseName: $"StreamerDbContext-{Guid.NewGuid()}")
+                .Options;
+
+            var streamerDbContextFake = new StreamerDbContext(options);
+            streamerDbContextFake.Videos!.AddRange(videos);
+            streamerDbContextFake.SaveChanges();
+
+            var mockRepository = new Mock<VideoRepository>(streamerDbContextFake);
+            //Removing this line since we're using InMemory EntityFramework
+            //mockRepository.Setup(r => r.GetAllAsync()).ReturnsAsync(videos);
             
             return mockRepository;
         }
